56ac239c353909a77f80fd700954ff0ffe3bd522
unknown <jmc.vilasboas2@gmail.com>
2024-10-31 21:57:51 -0300

    Primeiro Commit do Projeto, Testes iniciais + README

---
+++ README.md
@@ -0,0 +1,38 @@
+# Projeto de Testes com Cypress
+
+Este repositório contém um projeto de testes automatizados utilizando o Cypress. 
+
+## Descrição
+Este projeto realiza testes automatizados em um carrinho de compras na aplicação [Automation Exercise](https://automationexercise.com/) usando Cypress. O objetivo é garantir que os produtos possam ser adicionados corretamente ao carrinho e que a interface do usuário funcione como esperado.
+
+## Estrutura do Projeto
+
+- `cypress/fixtures`: Contém arquivos de dados que podem ser utilizados nos testes.
+- `cypress/support`: Contém arquivos de suporte e comandos personalizados para os testes.
+- `cypress/e2e`: Contém os testes de ponta a ponta.
+
+## Como Executar os Testes
+
+1. Clone este repositório.
+2. Navegue até o diretório do projeto.
+3. Execute `npm install` para instalar as dependências.
+4. Execute `npx cypress open` para abrir a interface do Cypress e rodar os testes.
+
+## Testes Realizados
+ - Adição de produtos ao carrinho.
+ - Verificação da visibilidade dos produtos no carrinho após a adição.
+ 
+ ## Futuras Melhorias
+ - Adicionar testes de navegação e formulários.
+ - Implementar integração contínua.
+ - Adicionar relatórios de testes e capturas de tela.
+
+
+
+## Contribuições
+
+Sinta-se à vontade para contribuir com melhorias e novos testes!
+
+
+## Licença
+Este projeto está licenciado sob a MIT License.
---
+++ cypress/e2e/teste-carrinho-de-compras.cy.js
@@ -0,0 +1,50 @@
+describe('Teste de Carrinho de Compras', () => {
+    let produtos = []; // Array para armazenar os nomes dos produtos
+
+    // Função que adiciona um produto ao carrinho.
+    const AdicionarProdutoAoCarrinho = (index) => {
+        cy.get('.productinfo.text-center').eq(index).within(() => {
+            cy.get('p').invoke('text').then((texto) => {
+                // Armazenar nome do produto
+                produtos.push(texto.trim());
+                // Log do produto sendo adicionado
+                cy.log(`Adicionando produto: ${texto.trim()}`); 
+                cy.contains('Add to cart').click();
+            });
+        });
+
+        // Espera o modal aparecer e o botão ser clicável
+        cy.get('.modal-footer').should('be.visible'); // Verifica se o modal está visível
+        cy.get('.modal-footer > .btn').click(); // Clica no botão de fechar o modal
+        cy.log('Produto adicionado ao carrinho e pop-up fechado.');
+    };
+
+    it('Teste de Adição de produtos ao Carrinho', () => {
+        // Navega até a página para os testes.
+        cy.visit('https://automationexercise.com/');
+        cy.log('Navegando para a página principal.');
+
+        cy.log('Adicionando Produtos...');
+        AdicionarProdutoAoCarrinho(1);
+        AdicionarProdutoAoCarrinho(3);
+        AdicionarProdutoAoCarrinho(5);
+
+        // Clica em Cart para visualizar produtos no carrinho.
+        cy.log('Navegando para o carrinho.');
+        cy.get(':nth-child(3) > a').contains('Cart').click();
+
+        // Verifica se a URL está correta
+        cy.url().should('include', 'view_cart');
+
+        // Segunda verificação, desta vez, verificando pela barra de navegação
+        cy.contains('Cart').should('be.visible');
+
+      // Validar que os produtos estão no carrinho
+      produtos.forEach((produto) => {
+        cy.contains(produto).should('be.visible').then(() => {
+            cy.log(`O produto "${produto}" está visível no carrinho.`); 
+        });
+    });
+        
+    });
+});
